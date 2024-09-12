48696fb5d0c8b3861bba346ceb416fbbd4c7c1b4
Anaïs Bernard <anais.mauricette.bernard@gmail.com>
2024-09-12 16:59:47 +0200

    Exercice 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,28 @@
+package ex1;
+
+/**
+ * Cette classe fournit des méthodes pour effectuer des opérations basiques
+ * comme l'addition et la soustraction.
+ */
+
+public class Calcul {
+
+    /**
+     * Additionne deux entiers.
+     * @return la somme de a et b
+     */
+
+    public int additionner(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Soustrait le deuxième entier du premier.
+     * @return la différence entre a et b
+     */
+
+    public int soustraire(int a, int b) {
+        return a - b;
+    }
+}
+
