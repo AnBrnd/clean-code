c8dccd0da4c0987fa3dfc618dfe1ae57a643cd1d
Anaïs Bernard <anais.mauricette.bernard@gmail.com>
2024-09-13 14:33:54 +0200

    Exercice 3

--- src/main/java/ex3/Zoo.java
+++ src/main/java/ex3/Zoo.java
@@ -1,49 +1,85 @@
 package ex3;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe représentant un zoo.
+ */
 public class Zoo {
 
     private String nom;
-    private List<String> types;
-    private List<String> noms;
-    private List<String> comportements;
+    private List<Animal> animaux;
 
     public Zoo(String nom) {
         this.nom = nom;
-    }
-
-    public void addAnimal(String nomAnimal, String typeAnimal, String comportement) {
-        types.add(typeAnimal);
-        noms.add(nomAnimal);
-        comportements.add(comportement);
-    }
-
-    public void afficherListeAnimaux() {
-        for (int i = 0; i < types.size(); i++) {
-            System.out.println(noms.get(i) + " " + types.get(i) + " " + comportements.get(i));
-        }
-    }
-
-    public int taille() {
-        return types.size();
+        this.animaux = new ArrayList<>();
     }
 
     /**
-     * Getter for nom
+     * Ajoute un animal au zoo.
      *
-     * @return the nom
+     * @param animal - animal à ajouter
      */
+    public void ajouterAnimal(Animal animal) {
+        animaux.add(animal);
+    }
+
+    /**
+     * Affiche la liste des animaux présents dans le zoo.
+     */
+    public void afficherListeAnimaux() {
+        for (Animal animal : animaux) {
+            System.out.println(animal);
+        }
+    }
+
+    /**
+     * Retourne la taille du zoo (le nombre d'animaux).
+     *
+     * @return - nombre d'animaux
+     */
+    public int taille() {
+        return animaux.size();
+    }
+
+    /**
+     * Retourne le nombre de mammifères présents dans le zoo.
+     *
+     * @return - nombre de mammifères
+     */
+    public int nombreMammiferes() {
+        int count = 0;
+        for (Animal animal : animaux) {
+            if ("MAMMIFERE".equals(animal.getType())) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Retourne le nombre de carnivores présents dans le zoo.
+     * @return - nombre de carnivores
+     */
+    public int nombreCarnivores() {
+        int count = 0;
+        for (Animal animal : animaux) {
+            if ("CARNIVORE".equals(animal.getComportement())) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+        // Getters et setters pour le nom
+
     public String getNom() {
         return nom;
     }
 
-    /**
-     * Setter
-     *
-     * @param nom the nom to set
-     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 }
+
--- src/main/java/ex3/ZooApplication.java
+++ src/main/java/ex3/ZooApplication.java
@@ -3,16 +3,20 @@ package ex3;
 public class ZooApplication {
 
     public static void main(String[] args) {
-        Zoo zoo = new Zoo("Thoiry");
+        Zoo zoo = new Zoo("La Flèche");
 
-        zoo.addAnimal("Gazelle", "MAMMIFERE", "HERBIVORE");
-        zoo.addAnimal("Zèbre", "MAMIFERE", "HERBIVORE");
-        zoo.addAnimal("Lion", "MAMMIFERE", "HERBIVORE");
-        zoo.addAnimal("Panthère", "MAMMIFERE", "CARNIVORE");
-        zoo.addAnimal("Requin blanc", "POISSON", "HERBIVORE");
-        zoo.addAnimal("Truite dorée", "POISSON", "HERBIVORE");
-        zoo.addAnimal("Boa constrictor", "SERPENT", "CARNIVORE");
-        zoo.addAnimal("Python", "SERPENT", "CARNIVORE");
+        zoo.ajouterAnimal(new Animal("Gazelle", "MAMMIFERE", "HERBIVORE"));
+        zoo.ajouterAnimal(new Animal("Zèbre", "MAMMIFERE", "HERBIVORE"));
+        zoo.ajouterAnimal(new Animal("Lion", "MAMMIFERE", "CARNIVORE"));
+        zoo.ajouterAnimal(new Animal("Panthère", "MAMMIFERE", "CARNIVORE"));
+        zoo.ajouterAnimal(new Animal("Requin blanc", "POISSON", "CARNIVORE"));
+        zoo.ajouterAnimal(new Animal("Truite dorée", "POISSON", "HERBIVORE"));
+        zoo.ajouterAnimal(new Animal("Boa constrictor", "SERPENT", "CARNIVORE"));
+        zoo.ajouterAnimal(new Animal("Python", "SERPENT", "CARNIVORE"));
+
+        zoo.afficherListeAnimaux();
+        System.out.println("Nombre d'animaux dans le zoo : " + zoo.taille());
+        System.out.println("Nombre de mammifères dans le zoo : " + zoo.nombreMammiferes());
+        System.out.println("Nombre de carnivores dans le zoo : " + zoo.nombreCarnivores());
     }
-
 }
