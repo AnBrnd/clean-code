da934366a538a2cdb722a1bb222a28de93c3b405
Anaïs Bernard <anais.mauricette.bernard@gmail.com>
2024-09-13 13:56:55 +0200

    Exercice 2

---
+++ src/main/java/ex2/CompteCourant.java
@@ -0,0 +1,33 @@
+package ex2;
+
+/**
+ * Représente un compte courant qui peut avoir un découvert
+ */
+
+public class CompteCourant extends CompteBancaire{
+
+    private double decouvertAutorise;
+
+    public CompteCourant(double solde, double decouvertAutorise) {
+        super(solde, decouvertAutorise);
+        this.decouvertAutorise = decouvertAutorise;
+    }
+
+    @Override
+    public void debiterMontant(double montant) {
+        if (this.solde - montant >= -decouvertAutorise) {
+            this.solde -= montant;
+        }
+        else {
+            System.out.println("Autorisation de découverte atteinte, débit refusé");
+        }
+    }
+
+    public double getDecouvertAutorise() {
+        return decouvertAutorise;
+    }
+
+    public void setDecouvertAutorise(double decouvertAutorise) {
+        this.decouvertAutorise = decouvertAutorise;
+    }
+}
--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,38 @@
 package ex2;
 
+/**
+ * Représente un compte bancaire de type Livret A qui ne peut pas avoir de découvert
+ */
+
 public class LivretA extends CompteBancaire {
 
-    public LivretA(String type, double solde, double tauxRemuneration) {
-        super(type, solde, 0, tauxRemuneration);
+    private double tauxRemuneration;
+
+    public LivretA(double solde, double tauxRemuneration) {
+        super(solde, 0);
+        this.tauxRemuneration = tauxRemuneration;
+    }
+
+    @Override
+
+    public void debiterMontant(double montant) {
+        if (this.solde - montant >= 0) {
+            this.solde -= montant;
+        } else {
+            System.out.println("Débit refusé : le livret A ne peut pas être à découvert.");
+        }
+    }
+
+    public void appliquerRemunerationAnnuelle() {
+        this.solde += this.solde * tauxRemuneration / 100;
+    }
+
+    public double getTauxRemuneration() {
+        return tauxRemuneration;
+    }
+
+    public void setTauxRemuneration(double tauxRemuneration) {
+        this.tauxRemuneration = tauxRemuneration;
     }
 
 }
